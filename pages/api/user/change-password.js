b01a2d61f0c3b5e3a88b593d254c667fde076423
Duong29ds <luuduong29112000@gmail.com>
2022-04-20 10:54:52 +0700

    first commit

---
+++ pages/api/user/change-password.js
@@ -0,0 +1,55 @@
+import { getSession } from "next-auth/client";
+import { hashPassword, verifyPassword } from "../../../lib/auth";
+import { connectToDatabase } from "../../../lib/db";
+
+async function handler(req, res) {
+  if (req.method !== "PATCH") {
+    return;
+  }
+
+  const session = await getSession({ req: req });
+
+  console.log(session);
+
+  if (!session) {
+    res.status(401).json({ message: "Not authenticated!" });
+    return;
+  }
+  const userEmail = session.user.email; //user is default in session get from getSession function
+  const oldPassword = req.body.oldPassword;
+  const newPassword = req.body.newPassword;
+
+  const client = await connectToDatabase();
+
+  const usersCollection = client.db().collection("users");
+
+  const user = await usersCollection.findOne({ email: userEmail });
+
+  if (!user) {
+    res.status(404).json({ message: "User not found" });
+    client.close();
+    return;
+  }
+
+  const currentPassword = user.password;
+
+  const passwordAreEqual = await verifyPassword(oldPassword, currentPassword);
+
+  if (!passwordAreEqual) {
+    res.status(403).json({ message: "Invalid password." });
+    client.close();
+    return;
+  }
+
+  const hashedPassword = await hashPassword(newPassword);
+
+  const result = await usersCollection.updateOne(
+    { email: userEmail },
+    { $set: { password: hashedPassword } }
+  ); // tham so dau tien de chi ra object, tham so thu 2 la thuoc tinh can update
+
+  client.close();
+  res.status(200).json({ message: "Password updated!" });
+}
+
+export default handler;
